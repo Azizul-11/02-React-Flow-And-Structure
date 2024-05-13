84fd5f5ad355c2f402ae10fcd8442e86df826c1e
Md Azizul Islam <mdazizul8741@gmail.com>
2024-05-13 20:22:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+// Day 2 
+
+Today i learn About vite-rect, that vite Rreact need Component name and function name in Capital letter (first letter) and extention name should be jsx.  
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import Index from './Index';
+
+const App = () => {
+  return (
+    <>
+    <Index/>
+    <h1>Hello How are You?</h1>
+    </>
+  )
+}
+
+export default App
